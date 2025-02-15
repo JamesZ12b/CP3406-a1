585a3d6c20ff3295567db4d9672b44fa3271793e
Jamesz12b <kaixin.zhan@my.jcu.edu.au>
2025-02-15 22:15:16 +0800

    Basic main screen design and addbook option

---
+++ app/src/main/java/com/example/booktracker/data/book.kt
@@ -0,0 +1,18 @@
+package com.example.booktracker.data
+
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+import com.example.booktracker.R
+
+data class Book(
+    @DrawableRes val imageResourceId: Int,
+    @StringRes val author: Int,
+    @StringRes val title: Int,
+    val pages: Int
+)
+
+val books = listOf(
+    Book(R.drawable.book_1, R.string.book_author_1,R.string.book_title_1,345),
+    Book(R.drawable.book_1, R.string.book_author_1,R.string.book_title_1,345),
+    Book(R.drawable.book_1, R.string.book_author_1,R.string.book_title_1,345)
+)
